87e6595b1ce749edc0902d11e6a61479917d1cfd
shevchenko <jorgeshevchenk@gmail.com>
2016-02-21 01:24:25 -0500

    act final

--- laravel/app/controllers/ReporteController.php
+++ laravel/app/controllers/ReporteController.php
@@ -270,4 +270,74 @@ class ReporteController extends BaseController
             )
         );
     }
+
+    public function postConsolidadonivel()
+    {
+        $data= Input::all();
+        $cargos=  DB::table('cargos')
+                    ->whereBetween('id', array(($data['nivel_id']+1), 9))->get();
+
+        $sum=array(); //inicializa array
+        $left="";
+        $campos="";
+        $cabecera="<tr>";
+        $cabecera.="<th>Paterno</th>";
+        $cabecera.="<th>Materno</th>";
+        $cabecera.="<th>Nombres</th>";
+        $datos="";
+        $cont=0;
+        array_push($sum,0);array_push($sum,0);array_push($sum,0);
+        foreach ($cargos as $key => $value) {
+            array_push($sum,0);
+            $cont++;
+            $cabecera.="<th style='text-align:center'> Nro <br>".$value->nombre."</th>";
+            $campos.=", COUNT(DISTINCT(a$cont.id)) id$cont";
+            $contant=($cont-1);
+                if($contant==0){
+                    $contant="";
+                }
+            $left.=" 
+                    LEFT JOIN activistas a$cont ON a$cont.lider_padre=a$contant.id AND a$cont.estado=1 ";
+        }
+        $cabecera.="<th>Total</th>";
+        $cabecera.="</tr>";
+        array_push($sum,0);
+
+        $sql="  SELECT a.paterno,a.materno,a.nombres $campos
+                FROM activistas a
+                $left
+                WHERE a.nivel_id=".$data['nivel_id']."
+                AND a.estado=1
+                GROUP BY a.id";
+
+        $r = DB::select($sql);
+
+        $inicio=0;
+            foreach ($r as $key => $value) {
+                $datos.="<tr>";
+                $total=0;
+                $inicio=0;
+                foreach ($value as $k => $v) {
+                    $total+=$v;
+                    $datos.="<td>".$v."</td>";
+                    $sum[$inicio]+=$v;
+                    $inicio++;
+                }
+                $datos.="<td>".$total."</td>";
+                $datos.="</tr>";
+            }
+            /*$datos.="<tr>";
+                foreach ($sum as $k => $v) { 
+                    $datos.="<td>".$v."</td>";
+                }
+            $datos.="</tr>";*/
+
+        return Response::json(
+            array(  'datos'=> $datos,
+                    'cabecera'=>$cabecera,
+                    'nro'=>$inicio,
+                    'rst' => 1
+            )
+        );
+    }
 }
