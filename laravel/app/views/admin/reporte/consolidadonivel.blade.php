18cddc0b21f9b64035b2b608d00b18748bbce034
shevchenko <jorgeshevchenk@gmail.com>
2016-02-21 02:38:12 -0500

    a

--- laravel/app/views/admin/reporte/consolidadonivel.blade.php
+++ laravel/app/views/admin/reporte/consolidadonivel.blade.php
@@ -18,7 +18,7 @@
             <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1 id="titulo">
-            Sin Red
+            Reporte Consolidado por Nivel
         </h1>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Admin</a></li>
