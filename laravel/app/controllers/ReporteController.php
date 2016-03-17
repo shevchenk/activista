27557f30bb60e3a2b6da3cff45f7bfa7c9d2fb05
shevchenko <jorgeshevchenk@gmail.com>
2016-03-17 02:16:59 -0500

    act

--- laravel/app/controllers/ReporteController.php
+++ laravel/app/controllers/ReporteController.php
@@ -4,7 +4,7 @@ class ReporteController extends BaseController
 {
     public function postPornivelgrupo()
     {
-        $grupo_persona_id= Input::get('grupos');
+        $grupo_persona_id= Input::get('grupo');
         $niveles= array(0,0,0,0,0,0,0,0,0,0);
         $paginas= array(0,0,0,0,0,0,0,0,0,0);
         $color=array('','#A4A4A4','#4BD1E0','#9B1E31','#45298F','#EFEFA2','#D3FFFF','#FF99FF','#D7F336','#8DB4E2');
@@ -33,7 +33,7 @@ class ReporteController extends BaseController
         $r=DB::select($sql);
         $ids=array();
         $html="";
-        $nivelanidado="N".$id;
+        //$nivelanidado="N".$id;
             foreach ($r as $key => $value) {
                 $html.="<tr style='background-color: ".$color[$value->nivel_id].";color: ".$colort[$value->nivel_id].";'>";
                     $html.="<td>".$value->nombre."</td>";
