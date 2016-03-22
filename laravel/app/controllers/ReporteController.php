376712a0da1f7da23d5ec87deba72a4861a89bb3
shevchenko <jorgeshevchenk@gmail.com>
2016-03-22 14:32:34 -0500

    act final

--- laravel/app/controllers/ReporteController.php
+++ laravel/app/controllers/ReporteController.php
@@ -328,8 +328,8 @@ class ReporteController extends BaseController
             $id= Input::get('persona');
             $nivel= Input::get('nivel');
         }
-        $niveles= array(0,0,0,0,0,0,0,0,0,0,0,0);
-        $paginas= array(0,0,0,0,0,0,0,0,0,0,0,0);
+        $niveles= array(0,0,0,0,0,0,0,0,0,0);
+        $paginas= array(0,0,0,0,0,0,0,0,0,0);
         $color=array('','#A4A4A4','#4BD1E0','#9B1E31','#45298F','#EFEFA2','#D3FFFF','#FF99FF','#D7F336','#8DB4E2','#A4A4A4','#A4A4A4','#A4A4A4');
         $colort=array('','#F0F0F0','#F0F0F0','#F0F0F0','#F0F0F0','#000000','#000000','#000000','#000000','#F0F0F0','#F0F0F0','#F0F0F0','#F0F0F0');
         $cargos=array();
